519bba5d6147cabe61f02d468e2f0ada8214b1da
Akhila Raju <akhilaraju@berkeley.edu>
2014-11-25 10:54:26 -0800

    added sample chart

--- sample.js
+++ sample.js
@@ -1,71 +1,70 @@
-d3.json("smallReview.json", function(d) {
-  mainvisdata = d
-  // var stars = mainvisdata["vcNAWiLM4dR7D2nwwJ7nCA"][0]["stars"]
-  // var time = mainvisdata["vcNAWiLM4dR7D2nwwJ7nCA"][0]["date"]
-  // console.log(stars)
-  // console.log(date)
-  //plot by date -- sorting algorithm required? 
-  //format: year-month-day (2013-05-14)
-});
+// d3.json("smallReview.json", function(d) {
+//   mainvisdata = d
+//   // var stars = mainvisdata["vcNAWiLM4dR7D2nwwJ7nCA"][0]["stars"]
+//   // var time = mainvisdata["vcNAWiLM4dR7D2nwwJ7nCA"][0]["date"]
+//   // console.log(stars)
+//   // console.log(date)
+//   //plot by date -- sorting algorithm required? 
+//   //format: year-month-day (2013-05-14)
+// });
 
-//date helper function
-function getDate(d) {
-	return new Date(d.jsonDate);
-}
+// //date helper function
+// function getDate(d) {
+// 	return new Date(d.jsonDate);
+// }
 
 //get max and min dates -
 var minDate = getDate(mainvisdata["vcNAWiLM4dR7D2nwwJ7nCA"][0]["date"])
 var maxDate = getDate(mainvisdata["vcNAWiLM4dR7D2nwwJ7nCA"][data.length-1]["date"])
    
-    var margin = {top: 20, right: 15, bottom: 60, left: 60}
-      , width = 960 - margin.left - margin.right
-      , height = 500 - margin.top - margin.bottom;
-    
-    var x = d3.time.scale()
-			  .domain([minDate, maxDate]).range([0, w]);
-              .range([ 0, width ]);
-    
-    var y = d3.scale.linear()
-    	      .domain([0, d3.max(data, function(d) { return d["vcNAWiLM4dR7D2nwwJ7nCA"][0]["date"]; })])
-    	      .range([ height, 0 ]);
- 
-    var chart = d3.select('body')
-	.append('svg:svg')
-	.attr('width', width + margin.right + margin.left)
-	.attr('height', height + margin.top + margin.bottom)
-	.attr('class', 'chart')
+    var data = [{"date":"2012-03-20","total":3},{"date":"2012-03-21","total":8},{"date":"2012-03-22","total":2},{"date":"2012-03-23","total":10},{"date":"2012-03-24","total":3},{"date":"2012-03-25","total":20},{"date":"2012-03-26","total":12}];
 
-    var main = chart.append('g')
-	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-	.attr('width', width)
-	.attr('height', height)
-	.attr('class', 'main')   
-        
-    // draw the x axis
-    var xAxis = d3.svg.axis()
-	.scale(x)
-	.orient('bottom');
+var margin = {top: 40, right: 40, bottom: 40, left:40},
+    width = 600,
+    height = 500;
 
-    main.append('g')
-	.attr('transform', 'translate(0,' + height + ')')
-	.attr('class', 'main axis date')
-	.call(xAxis);
+var x = d3.time.scale()
+    .domain([new Date(data[0].date), d3.time.day.offset(new Date(data[data.length - 1].date), 1)])
+    .rangeRound([0, width - margin.left - margin.right]);
 
-    // draw the y axis
-    var yAxis = d3.svg.axis()
-	.scale(y)
-	.orient('left');
+var y = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d.total; })])
+    .range([height - margin.top - margin.bottom, 0]);
 
-    main.append('g')
-	.attr('transform', 'translate(0,0)')
-	.attr('class', 'main axis date')
-	.call(yAxis);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .ticks(d3.time.days, 1)
+    .tickFormat(d3.time.format('%a %d'))
+    .tickSize(0)
+    .tickPadding(8);
 
-    var g = main.append("svg:g"); 
-    
-    g.selectAll("scatter-dots")
-      .data(data)
-      .enter().append("svg:circle")
-          .attr("cx", function (d,i) { return x(d["vcNAWiLM4dR7D2nwwJ7nCA"][0]["stars"]); } )
-          .attr("cy", function (d) { return y(d["vcNAWiLM4dR7D2nwwJ7nCA"][0]["date"]); } )
-          .attr("r", 5);
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left')
+    .tickPadding(8);
+
+var svg = d3.select('body').append('svg')
+    .attr('class', 'chart')
+    .attr('width', width)
+    .attr('height', height)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+svg.selectAll('.chart')
+    .data(data)
+  .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', function(d) { return x(new Date(d.date)); })
+    .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d.total)) })
+    .attr('width', 10)
+    .attr('height', function(d) { return height - margin.top - margin.bottom - y(d.total) });
+
+svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+    .call(xAxis);
+
+svg.append('g')
+  .attr('class', 'y axis')
+  .call(yAxis);
