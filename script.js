c0e7e5f2588d393259031977f904af78fe4a6d8a
Jimmy Yin <jimmy.yin@berkeley.edu>
2014-12-01 23:56:27 -0800

    smallmultiples
    not sure if it works correctly

--- script.js
+++ script.js
@@ -243,11 +243,11 @@ function viz(){
 
 
   //Set dimensions of canvas and graph
-  var margin = {top: 40, right: 40, bottom: 40, left:40},
-      width = 400,
-      height = 400,
-      padding = 1, // separation between nodes
-    radius = 4;
+var margin = {top: 40, right: 40, bottom: 40, left:40},
+    width = 400,
+    height = 400,
+    padding = 1, // separation between nodes
+  radius = 4;
 
 
 //Set ranges
@@ -396,4 +396,86 @@ controls.append("span")
       });
     };
   }
+}
+
+var intersection = function(array1, array2){
+  return array1.filter(function(n) {
+    return array2.indexOf(n) != -1
+  });
+}
+
+
+
+function smallMultiples(){
+  myBus = document.getElementById("businessTags").value;
+  if (! (myBus.indexOf(':') === -1)) { // handles duplicates
+    myBus = myBus.substring(0, myBus.indexOf(":"));
+  }
+  myData = data.filter(function(d){return d.name == myBus})[0]
+  myCats = myData.categories;
+  allData = data.filter(function(d){return intersection(myCats, d.categories).length > 0});
+
+  myCats.forEach(function(cat){
+    catData = allData.filter(function(d){return d.categories.indexOf(cat) != -1});
+    var min = catData[0].review_count;
+    var max = catData[catData.length-1].review_count;
+    var xVar = "review_count",
+        yVar = "stars";
+    var margin = {top: 40, right: 40, bottom: 40, left:40},
+                  width = 400,
+                  height = 400,
+                  padding = 1, // separation between nodes
+                  radius = 4;
+    //Set ranges
+    var x = d3.scale.linear()
+        .domain([min, max])
+        .rangeRound([0, width - margin.left - margin.right]);
+
+    var y = d3.scale.linear()
+        .domain([1, 5])
+        .range([height - margin.top - margin.bottom, 0]);
+
+    //Define the axes
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient('bottom')
+        .tickValues(x.domain())
+        .tickSize(0)
+        .tickPadding(8);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left')
+        .tickPadding(8)
+        .ticks(5);
+
+    x.domain(d3.extent(catData, function(d) { return d[xVar]; })).nice();
+    y.domain(d3.extent(catData, function(d) { return d[yVar]; })).nice();
+
+    var svg = d3.select('body').append('svg')
+                .attr('class', 'chart')
+                .attr('width', width)
+                .attr('height', height)
+              .append('g')
+                .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+    svg.selectAll("dot")
+        .data(catData)
+      .enter().append("circle")
+        .attr("r", 2.5)
+        .attr("cx", function(d) { return x(d[xVar]); })
+        .attr("cy", function(d) { return y(d[yVar]); });
+
+    //Add the X axis
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+        .call(xAxis);
+
+    //Add the Y axis
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis);
+
+  });
+
 }
